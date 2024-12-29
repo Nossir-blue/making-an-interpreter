3a8811c64f4d350101addf8dff836dcb7afcd318
Nossir-blue <rogerreis20@protonmail.com>
2024-12-29 09:26:21 +0100

    First Commit

---
+++ src/interpreter/Scanner.java
@@ -0,0 +1,61 @@
+package interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static interpreter.TokenType.*;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private Integer start = 0, current = 0, line = 1;
+
+    public Scanner(String source){
+        this.source = source;
+    }
+
+    public List<Token> scanTokens(){
+        while(!isAtEnd()){
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private Boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private void scanToken(){
+        Character c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+        }
+    }
+
+    private Character advance(){
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+
+}
