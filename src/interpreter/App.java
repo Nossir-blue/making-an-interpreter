8779b719ce6ee4c5b7547dbe90be015951fcdab0
Nossir-blue <rogerreis20@protonmail.com>
2025-01-08 18:27:09 +0100

    made the runFile and the ways to start the program

--- src/interpreter/App.java
+++ src/interpreter/App.java
@@ -1,11 +1,22 @@
 package interpreter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        
+        if(args.length > 1){
+            System.out.println("Usage: jLox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
-
-
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+    }
 }
