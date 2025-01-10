59ca5dd3be61b584e4d9f1dd19ebe6a36af69f5b
Nossir-blue <rogerreis20@protonmail.com>
2025-01-10 19:31:13 +0100

    redid the scanner

--- src/interpreter/App.java
+++ src/interpreter/App.java
@@ -1,12 +1,20 @@
 package interpreter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+
+
+import static interpreter.ErrorReporter.*;
+
 
 public class App {
-
-    public static void main(String[] args) throws Exception {
+    static boolean hadError = false;
+    public static void main(String[] args) throws IOException{
         if(args.length > 1){
             System.out.println("Usage: jLox [script]");
             System.exit(64);
@@ -16,7 +24,34 @@ public class App {
             runPrompt();
         }
     }
+
     private static void runFile(String path) throws IOException{
         byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code
+        if(hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens
+        for(Token token : tokens){
+            System.out.println(token);
+        }
     }
 }
