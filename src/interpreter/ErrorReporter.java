01ccec81b11eafcfd6fce84e8a474d89d0457707
Nossir-blue <rogerreis20@protonmail.com>
2025-01-10 19:31:29 +0100

    didn't add everything

---
+++ src/interpreter/ErrorReporter.java
@@ -0,0 +1,11 @@
+package interpreter;
+
+public class ErrorReporter {
+    
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+    private static void report(int line, String where, String message){
+        System.err.println("[Line " + line + "] Error" + where + ": " + message);
+    }
+}
