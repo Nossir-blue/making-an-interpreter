30f9821efe6f1917fd76f5dbeebcae9a7ca5a8bf
Nossir-blue <rogerreis20@protonmail.com>
2025-01-18 12:11:53 +0100

    adding more classes because i had no wifi

--- src/interpreter/AstPrinter.java
+++ src/interpreter/AstPrinter.java
@@ -11,23 +11,23 @@ public class AstPrinter implements Expr.Visitor<String>{
     }
 
     @Override
-    public String visitBinaryExpr(Binary expr) {
+    public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
     @Override
-    public String visitGroupingExpr(Grouping expr) {
+    public String visitGroupingExpr(Expr.Grouping expr) {
         return parenthesize("group", expr.expression);
     }
 
     @Override
-    public String visitLiteralExpr(Literal expr) {
+    public String visitLiteralExpr(Expr.Literal expr) {
         if(expr.value == null) return "nil";
         return expr.value.toString();
     }
 
     @Override
-    public String visitUnaryExpr(Unary expr) {
+    public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
@@ -43,4 +43,5 @@ public class AstPrinter implements Expr.Visitor<String>{
 
         return builder.toString();
     }
+
 }
